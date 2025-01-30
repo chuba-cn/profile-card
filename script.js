722753d5cafd22ac5cff51db1c32a175d4f8402b
chuba <chinemelumchubanwene57@gmail.com>
2025-01-30 01:03:52 +0100

    refactor: update utc-time getting function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function updateUTCTime() {
   const timeElement = document.getElementById("current-time");
   const now = new Date();
-  timeElement.textContent = now.toISOString();
+  timeElement.textContent = now.toUTCString().split(" ")[4];
 }
 
 updateUTCTime();
